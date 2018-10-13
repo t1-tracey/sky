1693f99048a8178ad5870eeb36fb918052698d32
Tracey <traceyle@outlook.com>
2018-10-13 13:51:45 +1100

    Initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+var synth = new Tone.AMSynth().toMaster()
+
+//attach a listener to all of the buttons
+document.querySelectorAll('button').forEach(function(button){
+	button.addEventListener('mousedown', function(e){
+		//play the note on mouse down
+		synth.triggerAttack(e.target.textContent)
+	})
+	button.addEventListener('mouseup', function(e){
+		//release on mouseup
+		synth.triggerRelease()
+    })
+    button.addEventListener('touchstart', function(e){
+		//play the note on mouse down
+		synth.triggerAttack(e.target.textContent)
+	})
+	button.addEventListener('touchend', function(e){
+		//release on mouseup
+		synth.triggerRelease()
+	})
+})
